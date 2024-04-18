87144a82ea50615f51b08cf0ef633d11c7489f40
rixmape <rixdonninorecario.mape@bicol-u.edu.ph>
2024-04-18 14:27:37 +0800

    Add module for helper functions

--- scripts/scrape_panlasang_pinoy.py
+++ scripts/scrape_panlasang_pinoy.py
@@ -1,35 +1,20 @@
-import argparse
+import asyncio
 import json
 import logging
-import asyncio
 from itertools import chain
 
 import aiohttp
-from bs4 import BeautifulSoup
+from helpers import (
+    configure_argument_parser,
+    extract_elements,
+    fetch_and_parse_html,
+    setup_logging,
+)
 
 BASE_URL = "https://panlasangpinoy.com/categories/recipes"
 INGREDIENT_CLASS = "wprm-recipe-ingredient-name"
 NAME_CLASS = "wprm-recipe-name"
 INSTRUCTION_CLASS = "wprm-recipe-instruction-text"
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-)
-
-
-async def fetch_and_parse_html(session, url):
-    """Fetch a webpage and return a parsed HTML."""
-    async with session.get(url) as response:
-        return BeautifulSoup(await response.text(), "html.parser")
-
-
-def extract_elements(container, class_name):
-    """Extract and return text from HTML elements based on class name."""
-    return [
-        element.text.strip()
-        for element in container.find_all(class_=class_name)
-    ]
 
 
 async def fetch_recipe_links(session, link):
@@ -63,32 +48,9 @@ async def fetch_recipe_details(session, link):
     return data
 
 
-def configure_argument_parser(description):
-    """Create and return a configured argument parser."""
-    parser = argparse.ArgumentParser(
-        description=description,
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-    )
-    parser.add_argument(
-        "-p",
-        "--pages",
-        help="Number of list pages to scrape",
-        type=int,
-        metavar="N",
-        required=True,
-    )
-    parser.add_argument(
-        "-o",
-        "--output",
-        help="Output file name",
-        metavar="FILE",
-        default="scraped_recipes.json",
-    )
-    return parser
-
-
 async def main():
     """Main entry point of the script."""
+    setup_logging()
     parser = configure_argument_parser("Scrape Panlasang Pinoy recipes")
     args = parser.parse_args()
 
